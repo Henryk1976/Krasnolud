f537c9e19062fa6f7df5fc850b0d45289d8f87cd
hbblo <hbblo@LAPTOP-UJKTBKSO>
2023-03-27 20:32:57 +0200

    Add project files.

---
+++ Krasnolud/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Krasnolud
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ile masz wzrostu w cm ?");
+
+            string x = Console.ReadLine();
+
+            int a = int.Parse(x);
+
+            if (a <= 140)
+            {
+                Console.WriteLine("Jesteś krasnoludem");
+            }
+            else if (a > 140 && a < 160)
+            {
+                Console.WriteLine("grasz w Barcelonie"); 
+            }
+            else { Console.WriteLine("w dowodzie będziesz miał napisane 'wysoki'");
+            }
+        }
+    }
+}
